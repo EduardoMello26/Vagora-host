4740a74db4466cb02ad545921e6d8f1c5aee2a23
Eduardo Imakawa <melimaka26@gmail.com>
2026-03-27 17:40:49 -0300

    Adição do Index inicial

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
+from django.views import View
+from django.contrib import messages
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'index.html')
+    def post(self, request):
+        pass
 
 # Create your views here.
