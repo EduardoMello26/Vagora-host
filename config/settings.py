3a5314716425ce22ab8d6b5b81a843fc004ed47b
Eduardo Imakawa <melimaka26@gmail.com>
2026-05-11 13:02:08 -0300

    Adição do env.example e do requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,10 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
@@ -126,8 +128,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'contadofarm26@gmail.com'
-EMAIL_HOST_PASSWORD = 'koju obwl baqt jzyf'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'contadofarm26@gmail.com')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER', 'contadofarm26@gmail.com')
 DEFAULT_FROM_EMAIL = 'contadofarm26@gmail.com'
 
 
